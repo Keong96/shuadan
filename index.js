2023c9ce3d93f5c725feb4297462e8c04f27c3cf
Keong96 <chinkeongtan96@gmail.com>
2025-12-28 12:10:39 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1067,8 +1067,8 @@ app.get('/users', verifyAdminToken, async (req, res) => {
       SELECT id, username, password, security_pin, phone, wallet_address, email, country, gender,
         TO_CHAR(dob, 'YYYY-MM-DD') AS dob, balance, draw_ticket, referral_code, referred_by,
         status, can_withdraw, can_do_task, user_type, vip_level, credit_score, last_login, created_at, is_demo, lucky_draw_setting,
-        (SELECT username FROM users WHERE id = referred_by) AS upline
-      FROM users
+        (SELECT username FROM users WHERE id = u.referred_by) AS upline
+      FROM users u
       WHERE user_type = 2 AND deleted_at IS NULL
     `;
     const params = [];
